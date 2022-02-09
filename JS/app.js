67cdb6540d3431cdc28c79bd885618336142c2a9
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-02-09 15:48:33 +0100

    [UP]
    Modification : rajout de balises span pour les erreurs dans le fichier html et modification du css nth-child changés en nth-of-type.

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,24 @@
-const dark = document.querySelector('div')
+const dark = document.querySelector('div');
+const valider = document.querySelector('button');
+const prenom = document.querySelector('.firstName');
+const nom = document.querySelector('.lastName');
+const mail = document.querySelector('.mail');
+const sujet = document.querySelector('.subject');
+const message = document.querySelector('.message')
 
 dark.addEventListener('click', function () {
     let body = document.getElementById("body");
     let currentClass = body.className;
     body.className = currentClass == "light-mode" ? "dark-mode" : "light-mode";
   })
+
+  valider.addEventListener('click', function (){
+      let valeur_prenom = prenom.value
+      let valeur_nom = nom.value
+      let valeur_mail = mail.value
+      let valeur_sujet = sujet.value
+      let valeur_message = message.value
+
+    console.log(valeur_prenom, valeur_nom, valeur_mail, valeur_sujet, valeur_message);
+  }
+  )
