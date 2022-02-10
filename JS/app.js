04a34957bcc4048b2549de61da58628bff50b8ac
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-02-10 14:16:57 +0100

    [UP]
    Modifications : ajout du @media pour le format tablette

--- JS/app.js
+++ JS/app.js
@@ -5,6 +5,7 @@ const nom = document.querySelector('.lastName');
 const mail = document.querySelector('.mail');
 const sujet = document.querySelector('.subject');
 const message = document.querySelector('.message')
+let error_first_name = document.getElementById('first_name')
 
 dark.addEventListener('click', function () {
     let body = document.getElementById("body");
@@ -12,12 +13,16 @@ dark.addEventListener('click', function () {
     body.className = currentClass == "light-mode" ? "dark-mode" : "light-mode";
   })
 
-  valider.addEventListener('click', function (){
-      let valeur_prenom = prenom.value
-      let valeur_nom = nom.value
-      let valeur_mail = mail.value
-      let valeur_sujet = sujet.value
-      let valeur_message = message.value
+valider.addEventListener('click', function (){
+    let valeur_prenom = prenom.value
+    let valeur_nom = nom.value
+    let valeur_mail = mail.value
+    let valeur_sujet = sujet.value
+    let valeur_message = message.value
+
+    //if(valeur_prenom = null){
+    //    error_first_name.
+    //}
 
     console.log(valeur_prenom, valeur_nom, valeur_mail, valeur_sujet, valeur_message);
   }
