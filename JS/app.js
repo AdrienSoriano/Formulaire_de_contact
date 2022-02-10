b86b558bc3a1a76ca4c6bf9382937eadf52abee4
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-02-10 15:30:12 +0100

    [UP]
    Modification : ajout des erreurs si champs vides avec JS

--- JS/app.js
+++ JS/app.js
@@ -4,8 +4,12 @@ const prenom = document.querySelector('.firstName');
 const nom = document.querySelector('.lastName');
 const mail = document.querySelector('.mail');
 const sujet = document.querySelector('.subject');
-const message = document.querySelector('.message')
-let error_first_name = document.getElementById('first_name')
+const message = document.querySelector('.message');
+let error_first_name = document.getElementById('first_name');
+let error_last_name = document.getElementById('last_name');
+let error_mail = document.getElementById('email');
+let error_subject = document.getElementById('subject');
+let error_message = document.getElementById('message');
 
 dark.addEventListener('click', function () {
     let body = document.getElementById("body");
@@ -13,17 +17,57 @@ dark.addEventListener('click', function () {
     body.className = currentClass == "light-mode" ? "dark-mode" : "light-mode";
   })
 
-valider.addEventListener('click', function (){
-    let valeur_prenom = prenom.value
-    let valeur_nom = nom.value
-    let valeur_mail = mail.value
-    let valeur_sujet = sujet.value
-    let valeur_message = message.value
+valider.addEventListener('click', function salut(){
+    let valeur_prenom = prenom.value;
+    let valeur_nom = nom.value;
+    let valeur_mail = mail.value;
+    let valeur_sujet = sujet.value;
+    let valeur_message = message.value;
+    let background_first_name = document.querySelector('.firstName');
+    let background_last_name = document.querySelector('.lastName');
+    let background_mail = document.querySelector('.mail');
+    let background_subject = document.querySelector('.subject');
+    let background_message = document.querySelector('.message');
+    
+    if(valeur_prenom == ""){
+        error_first_name.innerHTML = "Champ obligatoire.";
+        background_first_name.style.backgroundColor = 'rgba(255, 14, 14, 0.2)';
+    }
+    else{
+      error_first_name.innerHTML = "";
+      background_first_name.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
+    }
+    if(valeur_nom == ""){
+      error_last_name.innerHTML = "Champ obligatoire.";
+      background_last_name.style.backgroundColor = 'rgba(255, 14, 14, 0.2)';
+    }
+    else{
+      error_last_name.innerHTML = "";
+      background_last_name.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
+    }
+    if(valeur_mail == ""){
+      error_mail.innerHTML = "Champ obligatoire.";
+      background_mail.style.backgroundColor = 'rgba(255, 14, 14, 0.2)';
+    }
+    else{
+      error_mail.innerHTML = "";
+      background_mail.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
+    }
+    if(valeur_sujet == ""){
+      error_subject.innerHTML = "Champ obligatoire.";
+      background_subject.style.backgroundColor = 'rgba(255, 14, 14, 0.2)';
+    }
+    else{
+      error_subject.innerHTML = "";
+      background_subject.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
+    }
+    if(valeur_message == ""){
+      error_message.innerHTML = "Champ obligatoire.";
+      background_message.style.backgroundColor = 'rgba(255, 14, 14, 0.2)';
+    }
+    else{
+      error_message.innerHTML = "";
+      background_message.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
+    }
 
-    //if(valeur_prenom = null){
-    //    error_first_name.
-    //}
-
-    console.log(valeur_prenom, valeur_nom, valeur_mail, valeur_sujet, valeur_message);
-  }
-  )
+})
